dad9be75d592466cd7547f80f6c5beece5d744cf
Yan Martins <yan.martins@gmail.com>
2013-09-25 23:19:13 -0700

    Preparando formulário de contato

--- contact.php
+++ contact.php
@@ -17,22 +17,21 @@ $body_message .= 'Message: '.$field_message;
 $headers = 'From: '.$field_email."\r\n";
 $headers .= 'Reply-To: '.$field_email."\r\n";
 
-$mail_status = mail($mail_to, $subject, $body_message, $headers);
+//$mail_status = mail($mail_to, $subject, $body_message, $headers);
 
 if ($mail_status) { ?>
     <script language="javascript" type="text/javascript">
         alert('Thank you for the message. We will contact you shortly.');
-        window.location = '/contato.html';
+        window.location = '/contato.html?sent=true';
     </script>
 
 <?php
-
 }
 
 else { ?>
     <script language="javascript" type="text/javascript">
         alert('Message failed. Please, send an email to gordon@template-help.com');
-        window.location = '/contato.html';
+        window.location = '/contato.html?sent=false';
     </script>
 
 <?php
